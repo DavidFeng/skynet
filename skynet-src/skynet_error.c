a51695fd4bbd23b698162b1f01d5fb38350e1b43
David Feng <davidxifeng@gmail.com>
2016-08-09 19:42:39 +0800

    cache timestamp string

--- skynet-src/skynet_error.c
+++ skynet-src/skynet_error.c
@@ -19,14 +19,22 @@ static int prepend_timestamp(struct skynet_context * context, char buff[], size_
 	if (start_time == 0) {
 		start_time = skynet_starttime();
 	}
+	static uint64_t last_now = (uint64_t) -1;
+  static int r = 0;
 	uint64_t now = skynet_now();
+  if (last_now == now) {
+    return r;
+  } else {
+    last_now = now;
+  }
 	uint64_t tsp = now / 100 + start_time;
 	time_t t = (time_t) tsp;
 	struct tm tmr;
 	if (localtime_r(&t, &tmr) != NULL) {
 		int reslen = (int)strftime(buff, buffsize, "[%F %T", &tmr);
 		int n = snprintf(buff + reslen, buffsize - reslen, "-%02ld] ", now % 100);
-		return reslen + n;
+    r = reslen + n;
+		return r;
 	} else {
 		return 0;
 	}
@@ -42,7 +50,7 @@ skynet_error(struct skynet_context * context, const char *msg, ...) {
 		return;
 	}
 
-	char tmp[LOG_MESSAGE_SIZE];
+	static char tmp[LOG_MESSAGE_SIZE];
 	char *data;
 
 	int offset = prepend_timestamp(context, tmp, LOG_MESSAGE_SIZE);
